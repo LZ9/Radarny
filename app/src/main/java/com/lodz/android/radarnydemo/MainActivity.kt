3bc9af66055d398d086dded8ac066469bb7f03a1
zhouL <lz.mms@foxmail.com>
2022-11-07 14:38:11 +0800

    添加指标配置页面

--- app/src/main/java/com/lodz/android/radarnydemo/MainActivity.kt
+++ app/src/main/java/com/lodz/android/radarnydemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lodz.android.radarnydemo
 import android.os.Bundle
 import android.view.View
 import com.lodz.android.corekt.anko.getColorCompat
+import com.lodz.android.corekt.anko.toastShort
 import com.lodz.android.pandora.base.activity.BaseActivity
 import com.lodz.android.pandora.utils.viewbinding.bindingLayout
 import com.lodz.android.radarnydemo.databinding.ActivityMainBinding
@@ -13,6 +14,9 @@ class MainActivity : BaseActivity() {
 
     override fun getViewBindingLayout(): View = mBinding.root
 
+    /** 指标数 */
+    private var mCount = 0
+
     override fun findViews(savedInstanceState: Bundle?) {
         super.findViews(savedInstanceState)
         getTitleBarLayout().setTitleName(R.string.app_name)
@@ -20,8 +24,38 @@ class MainActivity : BaseActivity() {
         getTitleBarLayout().setBackgroundColor(getColorCompat(R.color.color_a9e0e2))
     }
 
+    override fun setListeners() {
+        super.setListeners()
+        mBinding.addBtn.setOnClickListener {
+            mCount += 1
+            mBinding.countEdit.setText(mCount.toString())
+        }
+
+        mBinding.deleteBtn.setOnClickListener {
+            mCount -= 1
+            mBinding.countEdit.setText(mCount.toString())
+        }
+
+        mBinding.updateBtn.setOnClickListener {
+            val text = mBinding.countEdit.text
+            if (text.isEmpty()) {
+                toastShort(R.string.main_input_hint)
+                return@setOnClickListener
+            }
+            var num = text.toString().toInt()
+            if (num < 3) {
+                num = 3
+                toastShort(R.string.main_correct_count)
+            }
+            mCount = num
+            mBinding.countEdit.setText(mCount.toString())
+            toastShort(mCount.toString())
+        }
+    }
+
     override fun initData() {
         super.initData()
+        mCount = mBinding.countEdit.text.toString().toInt()
         showStatusCompleted()
     }
 
