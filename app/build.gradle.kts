80f03a2cf7fe4bff53d81a96da418d8f801950c9
zhouL <lz.mms@foxmail.com>
2024-08-20 16:21:46 +0800

    升级工程

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.lodz.android.radarnydemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lodz.android.radarnydemo"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
--- radarny/build.gradle.kts
+++ radarny/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.lodz.android.radarny"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
