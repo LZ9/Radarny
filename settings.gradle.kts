177b840abcc5a8feff5b9e0d51c82b4c8b29d545
zhouL <lz.mms@foxmail.com>
2025-04-03 09:36:57 +0800

    替换使用镜像地址

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,26 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+//        google {
+//            content {
+//                includeGroupByRegex("com\\.android.*")
+//                includeGroupByRegex("com\\.google.*")
+//                includeGroupByRegex("androidx.*")
+//            }
+//        }
+//        mavenCentral()
+//        gradlePluginPortal()
 
-//        maven { setUrl("https://repo.huaweicloud.com/repository/maven") }
+        maven { setUrl("https://repo.huaweicloud.com/repository/maven") }
 
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
+//        google()
+//        mavenCentral()
 
-//        maven { setUrl("https://repo.huaweicloud.com/repository/maven") }
+        maven { setUrl("https://repo.huaweicloud.com/repository/maven") }
     }
 }
 
