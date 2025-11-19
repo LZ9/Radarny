a1042468f175e517bf85637c3a2012c1b10ec8c2
zhouL <lz.mms@foxmail.com>
2025-11-19 14:48:14 +0800

    使用com.vanniktech.maven.publish插件来上传依赖库

--- radarny/build.gradle.kts
+++ radarny/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
-//apply(from = "publish.gradle.kts")
 
 android {
     namespace = "com.lodz.android.radarny"
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         minSdk = 21
-        buildConfigField("int", "versionCode", "8")
-        buildConfigField("String", "versionName", "\"1.0.7\"") //成功上传
+        buildConfigField("int", "versionCode", "9")
+        buildConfigField("String", "versionName", "\"1.0.8\"") //成功上传
 
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -38,4 +38,43 @@ android {
 
 dependencies {
     implementation(libs.androidx.annotation)
+}
+
+//----------------------- 发布到 Maven Central  ------------------------------
+val PUBLISH_GROUP_ID = "ink.lodz"
+val PUBLISH_ARTIFACT_ID = "radarny"
+val PUBLISH_VERSION = "1.0.8" // 成功上传
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(PUBLISH_GROUP_ID, PUBLISH_ARTIFACT_ID, PUBLISH_VERSION)
+
+    pom {
+
+        name.set(PUBLISH_ARTIFACT_ID)
+        description.set("This is a simple radar chart component.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/LZ9/Radarny")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("Lodz")
+                name.set("Lodz")
+                email.set("lz.mms@foxmail.com")
+                url.set("https://github.com/LZ9")
+            }
+        }
+        scm {
+            url.set("https://github.com/LZ9/Radarny/tree/master")
+            connection.set("scm:git@github.com:LZ9/Radarny.git")
+            developerConnection.set("scm:git:ssh://github.com/LZ9/Radarny.git")
+        }
+    }
 }
